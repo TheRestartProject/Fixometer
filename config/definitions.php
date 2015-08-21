2f628ed77c97f5b56cd51f3dc9619c63d552240b
Alan Zard <tech@alanzard.com>
2015-08-21 09:25:08 +0200

    Public info text #22

--- config/definitions.php
+++ config/definitions.php
@@ -9,7 +9,7 @@ define('TBL_IMAGES',    5);
 define('TBL_LINKS',     6);
 
 /** shared text/interface elements **/
-define('WDG_PUBLIC_INFO', "The information input in this form will be published on the Restart Project website. Please be accurate and responsible.");
+define('WDG_PUBLIC_INFO', "Please be aware that information saved here will be published to The Restart Project's public website");
 
 /** Other Defs **/
 define('DEVICE_FIXED',      1);
