0e56ee4e67da0f46bd064fc9a6378693724ae8c9
Alan Zard <tech@alanzard.com>
2015-08-21 09:23:13 +0200

    Footer text and Logos #46

--- app/view/footer.php
+++ app/view/footer.php
@@ -4,14 +4,19 @@
             <div class="col-md-4">
                 <h5>The Fixometer</h5>
                 <p>
-                    is an open source project by <a href="http://therestartproject.org">the Restart Project.</a><br />
-                    Check it out on <a href="https://github.com/TheRestartProject/Fixometer">GitHub</a>.
+                   This tool was developed by The Restart Project with support from WRAP's Innovation in Waste Prevention Fund, in collaboration with The Engine Room and Circular Ecology. For technical support, please email <a href="mailto:janet@therestartproject.org">janet@therestartproject.org</a>
+                   
                     <br /><br />
                     Image on the login page &copy; <em>Heather Agyepong</em>
                 </p>
             </div>
-            <div class="col-md-5">&nbsp;</div>
-            <div class="col-md-3"><h5>Tech Specs</h5></div>
+            <div class="col-md-2">
+                <img src="/assets/images/circular_ecology.png" class="img-responsive" alt="Circular Ecology (Logo)">
+            </div>
+            <div class="col-md-2">
+                <img src="/assets/images/engineroom.png" class="img-responsive" alt="the engine room (Logo)">
+            </div>
+            <div class="col-md-4"><h5>Tech Specs</h5></div>
             </div>        
         </div>
     </footer>
