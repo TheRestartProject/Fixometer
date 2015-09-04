04db856291519b245f58b856f2042863b473e437
Alan Zard <tech@alanzard.com>
2015-09-04 11:50:30 +0200

    Footer tweaks

--- app/view/footer.php
+++ app/view/footer.php
@@ -1,22 +1,26 @@
     <footer id="main-footer">
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
-            <div class="col-md-4">
-                <h5>The Fixometer</h5>
+            <div class="col-md-6">
+                
+                <h3>The Fixometer</h3>
                 <p>
                    This tool was developed by The Restart Project with support from WRAP's Innovation in Waste Prevention Fund, in collaboration with The Engine Room and Circular Ecology. For technical support, please email <a href="mailto:janet@therestartproject.org">janet@therestartproject.org</a>
                    
                     <br /><br />
-                    Image on the login page &copy; <em>Heather Agyepong</em>
+                    Image on the login page &copy; <em class="blue">Heather Agyepong</em>
                 </p>
+            </div>
+            
+            <div class="col-md-2">
+                <img src="/assets/images/wrap-logo.png" class="img-responsive" alt="WRAP (Logo)">
+            </div>      
+            <div class="col-md-2">
+                <img src="/assets/images/engineroom.png" class="img-responsive" alt="the engine room (Logo)">
             </div>
             <div class="col-md-2">
                 <img src="/assets/images/circular_ecology.png" class="img-responsive" alt="Circular Ecology (Logo)">
             </div>
-            <div class="col-md-2">
-                <img src="/assets/images/engineroom.png" class="img-responsive" alt="the engine room (Logo)">
-            </div>
-            <div class="col-md-4"><h5>Tech Specs</h5></div>
-            </div>        
+            
         </div>
     </footer>
