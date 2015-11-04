cc7e9f349762c9d709c4d23a225b5dc8fb976b9a
Alan Zard <tech@alanzard.com>
2015-11-04 12:08:05 +0100

    Public dataviz stats

--- app/controller/admincontroller.ctrl.php
+++ app/controller/admincontroller.ctrl.php
@@ -7,7 +7,7 @@
             parent::__construct($model, $controller, $action);
             
             $Auth = new Auth($url);
-            if(!$Auth->isLoggedIn()){
+            if(!$Auth->isLoggedIn() && $action != 'stats'){
                 header('Location: /user/login');
             }
             else {
@@ -16,12 +16,160 @@
                 $this->set('user', $user);
                 $this->set('header', true);
                 
-                if(!hasRole($this->user, 'Administrator')) {
+                if(!hasRole($this->user, 'Administrator') &&  $action != 'stats') {
                     header('Location: /user/forbidden');
                 }
             }
         }
         
+        
+        public function stats(){
+            //Object Instances
+            $Group = new Group;
+            $User = new User;
+            $Party = new Party;
+            $Device = new Device;
+            
+            
+            $this->set('grouplist', $Group->findList());
+            
+            $allparties = $Party->ofThisGroup('admin', true, true);
+            
+            $participants = 0;
+            $hours_volunteered = 0;
+            
+            $need_attention = 0;
+            foreach($allparties as $i => $party){
+                if($party->device_count == 0){
+                    $need_attention++;    
+                }
+                
+                $party->co2 = 0;
+                $party->fixed_devices = 0;
+                $party->repairable_devices = 0;
+                $party->dead_devices = 0;
+                $party->guesstimates = false;
+                
+                $participants += $party->pax;
+                $hours_volunteered += (($party->volunteers > 0 ? $party->volunteers * 3 : 12 ) + 9);
+                
+                foreach($party->devices as $device){
+                    
+                    $party->co2 += $device->footprint;
+                    
+                    switch($device->repair_status){
+                        case 1:
+                            $party->fixed_devices++;
+                            break;
+                        case 2:
+                            $party->repairable_devices++;
+                            break;
+                        case 3:
+                            $party->dead_devices++;
+                            break;
+                    }
+                    if($device->category == 46){
+                        $party->guesstimates = true;
+                    }
+                }
+                
+                $party->co2 = number_format(round($party->co2 * $Device->displacement), 0, '.' , ',');    
+            }
+            $this->set('pax', $participants);
+            $this->set('hours', $hours_volunteered);
+            
+            $weights = $Device->getWeights();
+            $devices = $Device->ofAllGroups();
+            
+            $this->set('showbadges', $Device->guesstimates());
+            
+            $this->set('need_attention', $need_attention);
+            
+            $this->set('profile', $User->profilePage($this->user->id));
+            
+            $this->set('upcomingparties', $Party->findNextParties());
+            $this->set('allparties', $allparties);
+            
+            $this->set('devices', $devices); 
+            $this->set('weights', $weights);
+            
+            $this->set('device_count_status', $Device->statusCount());            
+            
+            
+            // more stats...
+            
+            /** co2 counters **/
+            $co2_years = $Device->countCO2ByYear();
+            $this->set('year_data', $co2_years);
+            $stats = array();
+            foreach($co2_years as $year){
+                $stats[$year->year] = $year->co2;
+            }
+            $this->set('bar_chart_stats', array_reverse($stats, true));
+            
+            $waste_years = $Device->countWasteByYear();
+            $this->set('waste_year_data', $waste_years);
+            $wstats = array();
+            foreach($waste_years as $year){
+                $wstats[$year->year] = $year->waste;
+            }
+            $this->set('waste_bar_chart_stats', array_reverse($wstats, true));
+            
+            
+            $co2Total = $Device->getWeights();
+            $co2ThisYear = $Device->countCO2ByYear(null, date('Y', time()));
+            
+            $this->set('co2Total', $co2Total[0]->total_footprints);
+            $this->set('co2ThisYear', $co2ThisYear[0]->co2);
+            
+            $wasteThisYear = $Device->countWasteByYear(null, date('Y', time()));
+            
+            $this->set('wasteTotal', $co2Total[0]->total_weights);
+            $this->set('wasteThisYear', $wasteThisYear[0]->waste);
+            
+            
+            $clusters = array();
+            
+            for($i = 1; $i <= 4; $i++) {
+                $cluster = $Device->countByCluster($i);
+                $total = 0;
+                foreach($cluster as $state){
+                    $total += $state->counter;
+                }
+                $cluster['total'] = $total;
+                $clusters['all'][$i] = $cluster;
+            }
+            
+            
+            for($y = date('Y', time()); $y>=2013; $y--){
+
+                for($i = 1; $i <= 4; $i++) {
+                    //$cluster = $Device->countByCluster($i, $group->idgroups);
+                    $cluster = $Device->countByCluster($i, null, $y);
+                    
+                    $total = 0;
+                    foreach($cluster as $state){
+                        $total += $state->counter;
+                    }
+                    $cluster['total'] = $total;
+                    $clusters[$y][$i] = $cluster;
+                }
+            }
+            $this->set('clusters', $clusters);
+            
+            // most/least stats for clusters
+            $mostleast = array();
+            for($i = 1; $i <= 4; $i++){
+                $mostleast[$i]['most_seen'] = $Device->findMostSeen(null, $i);
+                $mostleast[$i]['most_repaired'] = $Device->findMostSeen(1, $i);
+                $mostleast[$i]['least_repaired'] = $Device->findMostSeen(3, $i);
+                
+            }
+            
+            $this->set('mostleast', $mostleast);
+            $this->set('top', $Device->findMostSeen(1, null, null)); 
+            
+        }
         public function index(){
             
             $this->set('title', 'Administrator Dashboard');
