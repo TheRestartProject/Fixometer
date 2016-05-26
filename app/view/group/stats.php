166051b7c66a27ab18891b691c2d559e268ce269
Yavin <tech@alanzard.com>
2016-05-26 10:47:54 +0200

    Group mini stats

--- app/view/group/stats.php
+++ app/view/group/stats.php
@@ -65,4 +65,25 @@
         
     </div>
 
-<?php } ?>
+<?php } elseif($format == 'mini'){ ?>
+            <div id="group-main-stats">
+                <div class="col">
+                    <h5>parties thrown</h5>
+                    <span class="largetext"><?php echo $parties; ?></span>
+                </div>
+                
+                <div class="col">
+                    <h5>waste prevented</h5>
+                    <span class="largetext">
+                        <?php echo $waste; ?> kg 
+                    </span>
+                </div>
+                
+                <div class="col">
+                    <h5>CO<sub>2</sub> emission prevented</h5>
+                    
+                    <span class="largetext"><?php echo $co2; ?> kg</span>
+                </div>
+                
+            </div>
+<?php }?>
