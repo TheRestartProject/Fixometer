169373531e042b2da89a0c78c797b99c71eede62
Yavin <tech@alanzard.com>
2016-06-02 09:02:27 +0200

    Outbound

--- app/controller/outboundcontroller.ctrl.php
+++ app/controller/outboundcontroller.ctrl.php
@@ -12,10 +12,32 @@
         }
 		
 		public function index(){
+			
 			$counters = array();
 			$counters['groups'] = $this->groups->howMany();
 			$counters['parties'] = $this->parties->howMany();
+			$counters['pax'] = $this->parties->attendees();
+			
+			$allParties = $this->parties->ofThisGroup('admin', true, true);
+			$hours_volunteered = 0;
+			foreach($allParties as $party){
+				$hours_volunteered += (($party->volunteers > 0 ? $party->volunteers * 3 : 12 ) + 9);
+			}
+			$counters['hours'] = $hours_volunteered;
+			
+			$counters['devices'] = $this->devices->howMany();
+			$counters['statuses'] = $this->devices->statusCount();
+			$counters['most_seen'] = $this->devices->findMostSeen();
+			
+			$rates = array();
+			$rates['all'] = $this->devices->successRates();
+			for($i = 2012; $i <= date('Y'); $i++) {
+				$rates[$i] = $this->devices->successRates($i);
+			}
+			
+			
 			$this->set('counters', $counters);
+			$this->set('rates', $rates);
 			
 		}
 		
