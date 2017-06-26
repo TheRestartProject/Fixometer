65387fe1f3cfb1d1758e5f12064e9c8fd3b9345c
Neil Mather <ngmather@gmail.com>
2017-06-26 13:48:12 +0100

    Move explainer text for password recovery page.

--- app/view/user/recover.php
+++ app/view/user/recover.php
@@ -2,10 +2,12 @@
     <div class="text-center">
         <img src="/assets/images/logo_mini.png" alt="The Restart Project">
     </div>
-<p class="login-text">Please input the email address you used to register with the Fixometer. The system will send you an email with instructions on how to recover access to your account. </p>
+    <p class="login-text"><span class="patua-blue">Welcome to our community space,</span> where you can share upcoming Restart Parties and track their social and environmental impact. By doing so, we can empower and motivate at a local level, but also build global momentum for a change.</p>
     <?php if(isset($response)) { printResponse($response); } ?>
     <div class="shader"></div>
     <h2><span class="title-text">recover your password</span></h2>
+
+    <p class="explainer-text">Please input the email address that was used when you first registered with the Fixometer. The system will send you an email with instructions on how to recover the access to your account. </p>
 
     <form class="" method="post" action="/user/recover">
 
