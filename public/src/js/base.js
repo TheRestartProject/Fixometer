a87c8fc236d6b621e8e42de1525056c702142710
Neil Mather <ngmather@gmail.com>
2018-01-22 22:50:05 +0000

    Typo

--- public/src/js/base.js
+++ public/src/js/base.js
@@ -86,7 +86,7 @@ $('.device-image-delete').click(function(e){
                                   '<small>Please input an estimate weight (in kg)</small>' +
                                   '<div class="input-group">' +
                                   '<input type="text" name="device[' + n +'][estimate]" id="device[' + n +'][estimate]" class="form-control" placeholder="Estimate...">' +
-                                  '<span class="input-group-addon>kg</span>' +
+                                  '<span class="input-group-addon">kg</span>' +
                                   '</div>' +
                               '</div>' +
                           '</td>' +
