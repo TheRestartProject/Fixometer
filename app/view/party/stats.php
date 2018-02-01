c2c412777608d2aacc2023d1be440b0831402878
Neil Mather <ngmather@gmail.com>
2018-02-01 12:49:40 +0000

    Responsiveness of embedded stats

--- app/view/party/stats.php
+++ app/view/party/stats.php
@@ -1,64 +1,47 @@
-<div class="row">
+<div class="" id="party-headline-stats">
 
-    <div class="col-xs-4 col-sm-2 stat">
-        <div class="row">
-            <div class="col-sm-6" style="height:80px">
+    <div class="stat1">
+        <span>
                 <i class="fa fa-group"></i>
                 <span class="subtext">participants</span>
-            </div>
-            <div class="col-sm-6 vcenter">
-                <?php echo $party->pax; ?>
-            </div>
-        </div>
+        </span>
+        <?php echo $party->pax; ?>
     </div>
 
-    <div class="col-xs-4 col-sm-2 stat">
-        <div class="row">
-            <div class="col-sm-6">
+    <div class="stat1">
+        <span>
                 <img class="" alt="The Restart Project: Logo" src="/assets/images/logo_mini.png">
                 <span class="subtext">restarters</span>
-            </div>
-            <div class="col-sm-6 vcenter">
+            </span>
                 <?php echo $party->volunteers; ?>
-            </div>
-        </div>
     </div>
 
-    <div class="col-xs-4 col-sm-2 stat">
+    <div class="stat1">
         <div class="footprint">
+            <div>
             <span id="co2-diverted-value"><?php echo $party->co2; ?></span>
             <span class="subtext">kg of CO<sub>2</sub></span>
-            <br />
+            </div>
+            <div>
             <span id="ewaste-diverted-value"><?php echo number_format($party->ewaste, 0); ?></span>
             <span class="subtext">kg of waste</span>
-        </div>
-    </div>
-
-    <div class="col-xs-4 col-sm-2 stat">
-        <div class="row">
-            <div class="col-sm-6"><i class="status mid fixed"></i></div>
-            <div class="col-sm-6 vcenter">
-                <?php echo $party->fixed_devices; ?>
             </div>
         </div>
     </div>
 
-    <div class="col-xs-4 col-sm-2 stat">
-        <div class="row">
-            <div class="col-sm-6"><i class="status mid repairable"></i></div>
-            <div class="col-sm-6 vcenter">
-                <?php echo $party->repairable_devices; ?>
-            </div>
-        </div>
+    <div class="stat1">
+            <i class="status-inline mid fixed"></i>
+            <span class="fixed"><?php echo $party->fixed_devices; ?></span>
     </div>
 
-    <div class="col-xs-4 col-sm-2 stat">
-        <div class="row">
-            <div class="col-sm-6"><i class="status mid dead"></i></div>
-            <div class="col-sm-6 vcenter">
-                <?php echo $party->dead_devices; ?>
-            </div>
-        </div>
+    <div class="stat1">
+            <i class="status-inline mid repairable"></i>
+            <span class="repairable"><?php echo $party->repairable_devices; ?></span>
+    </div>
+
+    <div class="stat1">
+            <i class="status-inline mid dead"></i>
+            <span class="dead"><?php echo $party->dead_devices; ?></span>
     </div>
 
 </div>
