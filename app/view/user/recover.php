2961aaa7ed5961ae266daa876aa565c624573d87
NikhitaPaturu <nikhitapaturu9@gmail.com>
2018-03-12 11:22:05 +0000

    Stats summary is now available in recovery page also

--- app/view/user/recover.php
+++ app/view/user/recover.php
@@ -22,44 +22,44 @@
     </form>
 </div>
 <?php
-/** select random bkground **/
-$rand = rand(1, 4);
-?>
-<div class="login-deets bg_<?php echo $rand; ?>">
+        /** select random bkground **/
+        $rand = rand(1, 4);
+        ?>
+        <div class="login-deets bg_<?php echo $rand; ?>">
 
 
-        <div class="detail-wrap">
-            <div class="detail">
-                <h4><?php _t('Devices Restarted')?></h4>
-                <span class="big-number">
-                    <span class="big-number"><?php echo number_format($devices[0]->counter, 0, '-', ','); ?></span>
-                </span>
-            </div>
+                <div class="detail-wrap">
+                    <div class="detail">
+                        <h4><?php _t('Devices Restarted')?></h4>
+                        <span class="big-number">
+                            <span id="devices-restarted-value" class="big-number"><?php echo number_format($devices[0]->counter, 0, '-', ','); ?></span>
+                        </span>
+                    </div>
 
-            <div class="detail">
-                <h4><?php _t("CO<sub>2</sub> Emission prevented") ?></h4>
-                <span class="big-number"><?php echo number_format($weights[0]->total_footprints, 0, '-', ','); ?> kg</span>
-            </div>
-            <div class="detail">
-                <h4><?php _t('Waste prevented')?></h4>
-                <span class="big-number"><?php echo number_format($weights[0]->total_weights, 0, '-', ','); ?> kg</span>
-            </div>
+                    <div class="detail">
+                        <h4><?php _t("CO<sub>2</sub> Emission prevented") ?></h4>
+                        <span id="co2-prevented-value" class="big-number"><?php echo number_format($weights[0]->total_footprints, 0, '-', ','); ?> kg</span>
+                    </div>
+                    <div class="detail">
+                        <h4><?php _t('Waste prevented')?></h4>
+                        <span id="waste-prevented-value" class="big-number"><?php echo number_format($weights[0]->total_weights, 0, '-', ','); ?> kg</span>
+                    </div>
 
 
-            <div class="detail">
-                <h4><?php _t('Parties thrown') ?></h4>
-                <span class="big-number"><?php echo count($allparties) - count($nextparties); ?></span>
-            </div>
+                    <div class="detail">
+                        <h4><?php _t('Parties thrown') ?></h4>
+                        <span id="parties-thrown-value" class="big-number"><?php echo count($allparties) - count($nextparties); ?></span>
+                    </div>
 
-        </div>
-
-</div>
-<div class="container">
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+                </div>
 
         </div>
+<<div class="container">
 
-    </div>
-</div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+
+                </div>
+
+            </div>
+        </div>
